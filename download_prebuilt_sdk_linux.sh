58ec613a56b59e0a53adf5b10247bbb24fa36153
Lazy_V <954294627@qq.com>
2024-02-16 19:41:43 +0000

    add linux x86_64 download script

---
+++ download_prebuilt_sdk_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# create tmp folder
+tmpFolder="tmp"
+mkdir -p "$tmpFolder"
+
+# setup download url link and file name
+url="https://github.com/SnowLeopardEngine/SPhysX-Cross/releases/download/Prebuilt-linux-x86_64/Prebuilt_linux_x86_64.zip"
+zipFileName="Prebuilt_linux_x86_64.zip"
+
+# prepare folder names
+downloadDirectory="$tmpFolder"
+extractDirectory="$downloadDirectory"
+
+# if .zip not exist.
+if [ ! -f "$downloadDirectory/$zipFileName" ]; then
+    echo "Start downloading PhysX Prebuilt SDK..."
+    # download the file
+    curl -o "$downloadDirectory/$zipFileName" "$url" -L
+    echo "Finished downloading PhysX Prebuilt SDK."
+fi
+
+if [ ! -d "$extractDirectory/Prebuilt" ]; then
+    echo "Start extracting PhysX Prebuilt SDK..."
+    # extract file
+    unzip -o "$downloadDirectory/$zipFileName" -d "$extractDirectory"
+    echo "Finished extracting PhysX Prebuilt SDK."
+fi
+
+if [ ! -d "Prebuilt" ]; then
+    # copy prebuilt files
+    cp -r "$extractDirectory/Prebuilt" "Prebuilt"
+    echo "Finished copying PhysX Prebuilt SDK to project root."
+fi
